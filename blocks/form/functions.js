1c1aa33230eee3a5c7cfaac323f6283ad168b68a
Venu Vustipalli <venu.vustipalli@concentrix.com>
2025-08-18 16:20:54 -0500

    Added fetchProviders custom rule function

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -42,5 +42,33 @@ function days(endDate, startDate) {
   return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
+/**
+ * @name fetchProviders
+ * @param {string} apiUrl - REST endpoint URL
+ * @param {scope} globals - Form context (auto injected)
+ * @return {string[]} - Provider names
+ */
+function fetchProviders(apiUrl, globals) {
+  fetch(apiUrl)
+    .then(resp => resp.json())
+    .then(data => {
+      if (data && Array.isArray(data.Providers)) {
+        const providerNames = data.Providers.map(p => p.provider_name);
+
+        const field = globals.field;
+
+        if (field) {
+          field.items = providerNames.map(name => ({
+            displayText: name,
+            value: name
+          }));
+        }
+      }
+    })
+    .catch(err => console.error("Error fetching providers:", err));
+
+  return [];
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName, days, submitFormArrayToString };
+export { getFullName, days, submitFormArrayToString, fetchProviders };
